a4184af78251007f0ab8f969c88adcfc39dca8a0
x0thra <x0thra@users.noreply.github.com>
2026-08-17 08:58:46 +0300

    added cowsay lol

--- src/App.svelte
+++ src/App.svelte
@@ -247,7 +247,19 @@
 
     switch (mainCommand) {
       case 'help':
-        output = 'Available commands:\n  help      - Show this message\n  ls (ll)   - List files and directories\n  cd        - Change directory\n  pwd       - Print working directory\n  cat       - View file content\n  fastfetch - Display system info\n  date      - Print current date and time\n  uname     - Print system info\n  echo      - Print a message\n  touch     - Create an empty file\n  nano      - Text editor\n  history   - Command history\n  clear     - Clear terminal\n  whoami    - Print current user\n  matrix    - Run the matrix digital rain';
+        output = 'Available commands:\n  help      - Show this message\n  ls (ll)   - List files and directories\n  cd        - Change directory\n  pwd       - Print working directory\n  cat       - View file content\n  fastfetch - Display system info\n  date      - Print current date and time\n  uname     - Print system info\n  echo      - Print a message\n  touch     - Create an empty file\n  nano      - Text editor\n  history   - Command history\n  cowsay    - A talking cow\n  clear     - Clear terminal\n  whoami    - Print current user\n  matrix    - Run the matrix digital rain';
+        break;
+      case 'cowsay':
+        const textToSay = args.slice(1).join(' ') || "Moo";
+        const borderLine = '-'.repeat(textToSay.length + 2);
+        output = ` ${'_'.repeat(textToSay.length + 2)}
+< ${textToSay} >
+ ${borderLine}
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`;
         break;
       case 'ls':
       case 'll':
